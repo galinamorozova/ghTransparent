73bbfdc694ecd11dcd720acc512792a730d53d45
galinamorozova <41506249+galinamorozova@users.noreply.github.com>
2022-05-17 11:20:27 +0300

    Add files via upload

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -2,23 +2,23 @@ import './video.scss';
 import {React, useState} from "react";
 import Slider from "react-slick";
 import video from "./../../images/vid1.mp4"
-import video2 from "./../../images/vid2.mp4"
-import video3 from "./../../images/vid3.mp4"
-// import tit1 from "./../../images/1.jpeg"
-// import tit2 from "./../../images/2.jpeg"
-// import tit3 from "./../../images/3.jpeg"
-// import tit4 from "./../../images/4.jpeg"
-// import tit5 from "./../../images/5.jpeg"
-// import tit6 from "./../../images/6.jpeg"
-// import tit7 from "./../../images/7.jpeg"
-// import tit8 from "./../../images/8.jpeg"
-// import tit9 from "./../../images/9.jpeg"
-// import tit10 from "./../../images/10.jpeg"
-// import tit11 from "./../../images/11.jpeg"
-// import tit12 from "./../../images/12.jpeg"
-// import tit13 from "./../../images/13.jpeg"
-// import tit14 from "./../../images/14.jpeg"
-// import tit15 from "./../../images/15.jpeg"
+// import video2 from "./../../images/vid2.mp4"
+// import video3 from "./../../images/vid3.mp4"
+// import tit1 from "./../../images/1.webp"
+// import tit2 from "./../../images/2.webp"
+// import tit3 from "./../../images/3.webp"
+// import tit4 from "./../../images/4.webp"
+// import tit5 from "./../../images/5.webp"
+// import tit6 from "./../../images/6.webp"
+// import tit7 from "./../../images/7.webp"
+// import tit8 from "./../../images/8.webp"
+// import tit9 from "./../../images/9.webp"
+// import tit10 from "./../../images/10.webp"
+// import tit11 from "./../../images/11.webp"
+// import tit12 from "./../../images/12.webp"
+// import tit13 from "./../../images/13.webp"
+// import tit14 from "./../../images/14.webp"
+// import tit15 from "./../../images/15.webp"
 
 const images = [
     {src: "https://i.ibb.co/L0f9tp2/1.jpg"},
@@ -67,23 +67,40 @@ export default function Video() {
         slidesToShow: 5,
         slidesToScroll: 1,
     };
+
+    const settings3 = {
+        dots: true,
+        arrows:true,
+        infinite: true,
+        lazyLoad: true,
+        fade: true,
+        swipe: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
     return (
         <div className='video'>
             <div className="container">
                 <div className="forMobile">
                     <Slider
-                        asNavFor={nav2}
-                        ref={slider => (setNav1(slider))}
-                        {...settings}>
-                        <div className="pictureContainer forMobile">
-                            <img src="https://i.ibb.co/L0f9tp2/1.jpg" alt="прозрачные роллеты" className="picture"/>
-                        </div>
-                        <div className="pictureContainer forMobile">
-                            <img src="https://i.ibb.co/XsHJmZ2/2.jpg" alt="прозрачные роллеты" className="picture"/>
-                        </div>
-                        <div className="pictureContainer forMobile">
-                            <img src="https://i.ibb.co/qk2PndZ/3.jpg" alt="прозрачные роллеты" className="picture"/>
-                        </div>
+                        {...settings3}>
+                        {/*<div className="pictureContainer forMobile">*/}
+                        {/*    <img src="https://i.ibb.co/L0f9tp2/1.jpg" alt="прозрачные роллеты" className="picture"/>*/}
+                        {/*</div>*/}
+                        {/*<div className="pictureContainer forMobile">*/}
+                        {/*    <img src="https://i.ibb.co/XsHJmZ2/2.jpg" alt="прозрачные роллеты" className="picture"/>*/}
+                        {/*</div>*/}
+                        {/*<div className="pictureContainer forMobile">*/}
+                        {/*    <img src="https://i.ibb.co/qk2PndZ/3.jpg" alt="прозрачные роллеты" className="picture"/>*/}
+                        {/*</div>*/}
+                        {/*<iframe className="videoContent"*/}
+                        {/*        loading="lazy"*/}
+                        {/*        title="aluminservice.ru"*/}
+                        {/*        src="https://www.youtube.com/embed/9_O_HjClwLM"*/}
+                        {/*        allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope"*/}
+                        {/*        allowFullScreen></iframe>*/}
                         <div>
                             <video className="videoContent" id="video" controls autoPlay muted>
                                 <source src={video} type="video/mp4"/>
@@ -95,52 +112,16 @@ export default function Video() {
                                 <img src={img.src} alt="прозрачные роллеты" className="picture"/>
                             </div>
                         })}
-                        <div>
-                            <video className="videoContent" controls autoPlay muted>
-                                <source src={video2} type="video/mp4"/>
-                            </video>
-                        </div>
-                        <div>
-                            <video className="videoContent" controls autoPlay muted>
-                                <source src={video3} type="video/mp4"/>
-                            </video>
-                        </div>
-                    </Slider>
-                    <Slider
-                        asNavFor={nav1}
-                        ref={slider => (setNav2(slider))}
-                        swipeToSlide={true}
-                        focusOnSelect={true}
-                        {...settings2}>
-                        <div className="pictureContainer2 forMobile">
-                            <img src="https://i.ibb.co/L0f9tp2/1.jpg" alt="прозрачные роллеты" className="picture2"/>
-                        </div>
-                        <div className="pictureContainer2 forMobile">
-                            <img src="https://i.ibb.co/XsHJmZ2/2.jpg" alt="прозрачные роллеты" className="picture2"/>
-                        </div>
-                        <div className="pictureContainer2 forMobile">
-                            <img src="https://i.ibb.co/qk2PndZ/3.jpg" alt="прозрачные роллеты" className="picture2"/>
-                        </div>
-                        <div>
-                            <video className="videoContent2" controls muted>
-                                <source src={video} type="video/mp4"/>
-                            </video>
-                        </div>
-                        {images.map(img => {
-                            return <div className="pictureContainer2">
-                                <img src={img.src} alt="прозрачные роллеты" className="picture2"/>
-                            </div>
-                        })}
-                        <div>
-                            <video className="videoContent2" controls muted>
-                                <source src={video2} type="video/mp4"/>
-                            </video>
-                        </div>
-                        <div>
-                            <video className="videoContent2" width="95" height="95" controls muted>
-                                <source src={video3} type="video/mp4"/>
-                            </video>
-                        </div>
+                        {/*<div>*/}
+                        {/*    <video className="videoContent" controls autoPlay muted>*/}
+                        {/*        <source src={video2} type="video/mp4"/>*/}
+                        {/*    </video>*/}
+                        {/*</div>*/}
+                        {/*<div>*/}
+                        {/*    <video className="videoContent" controls autoPlay muted>*/}
+                        {/*        <source src={video3} type="video/mp4"/>*/}
+                        {/*    </video>*/}
+                        {/*</div>*/}
                     </Slider>
                 </div>
                 <div className="forDesktop">
@@ -148,6 +129,12 @@ export default function Video() {
                         asNavFor={nav2}
                         ref={slider => (setNav1(slider))}
                         {...settings}>
+                        {/*<iframe className="videoContent"*/}
+                        {/*        loading="lazy"*/}
+                        {/*        title="aluminservice.ru"*/}
+                        {/*        src="https://www.youtube.com/embed/9_O_HjClwLM"*/}
+                        {/*        allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope"*/}
+                        {/*        allowFullScreen></iframe>*/}
                         <div>
                             <video className="videoContent" id="video" controls autoPlay muted>
                                 <source src={video} type="video/mp4"/>
@@ -159,16 +146,16 @@ export default function Video() {
                                 <img src={img.src} alt="прозрачные роллеты" className="picture"/>
                             </div>
                         })}
-                        <div>
-                            <video className="videoContent" controls autoPlay muted>
-                                <source src={video2} type="video/mp4"/>
-                            </video>
-                        </div>
-                        <div>
-                            <video className="videoContent" controls autoPlay muted>
-                                <source src={video3} type="video/mp4"/>
-                            </video>
-                        </div>
+                        {/*<div>*/}
+                        {/*    <video className="videoContent" controls autoPlay muted>*/}
+                        {/*        <source src={video2} type="video/mp4"/>*/}
+                        {/*    </video>*/}
+                        {/*</div>*/}
+                        {/*<div>*/}
+                        {/*    <video className="videoContent" controls autoPlay muted>*/}
+                        {/*        <source src={video3} type="video/mp4"/>*/}
+                        {/*    </video>*/}
+                        {/*</div>*/}
                     </Slider>
                     <Slider
                         asNavFor={nav1}
@@ -176,6 +163,13 @@ export default function Video() {
                         swipeToSlide={true}
                         focusOnSelect={true}
                         {...settings2}>
+                        {/*<iframe className="videoContent2"*/}
+                        {/*        loading="lazy"*/}
+                        {/*        title="aluminservice.ru"*/}
+                        {/*        src="https://www.youtube.com/embed/9_O_HjClwLM"*/}
+                        {/*        frameBorder="0"*/}
+                        {/*        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope"*/}
+                        {/*        allowFullScreen></iframe>*/}
                         <div>
                             <video className="videoContent2" controls muted>
                                 <source src={video} type="video/mp4"/>
@@ -186,16 +180,16 @@ export default function Video() {
                                 <img src={img.src} alt="прозрачные роллеты" className="picture2"/>
                             </div>
                         })}
-                        <div>
-                            <video className="videoContent2" controls muted>
-                                <source src={video2} type="video/mp4"/>
-                            </video>
-                        </div>
-                        <div>
-                            <video className="videoContent2" width="95" height="95" controls muted>
-                                <source src={video3} type="video/mp4"/>
-                            </video>
-                        </div>
+                        {/*<div>*/}
+                        {/*    <video className="videoContent2" controls muted>*/}
+                        {/*        <source src={video2} type="video/mp4"/>*/}
+                        {/*    </video>*/}
+                        {/*</div>*/}
+                        {/*<div>*/}
+                        {/*    <video className="videoContent2" width="95" height="95" controls muted>*/}
+                        {/*        <source src={video3} type="video/mp4"/>*/}
+                        {/*    </video>*/}
+                        {/*</div>*/}
                     </Slider>
                 </div>
 
